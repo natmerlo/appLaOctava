8a6ef9b92d88fceb27cfbad532c022ee43b6d209
natalia <natalia.merlo@davinci.edu.ar>
2024-04-16 10:44:23 -0300

    Index panel administración

--- resources/views/livewire/componentes/paneladmin/listproductos.blade.php
+++ resources/views/livewire/componentes/paneladmin/listproductos.blade.php
@@ -1,38 +1,67 @@
 <div>
-      
+
       <div class="container mx-auto">
-            <div class="flex justify-between items-center">
-                  <span class="text-[2.5rem] font-extrabold">PRODUCTOS</span>
+            <div class="flex items-center justify-between">
+                  <span class="text-[2.5rem] font-extrabold eucrosia">Productos</span>
                   @livewire('componentes.paneladmin.productonuevo')
             </div>
 
             <div class="flex flex-wrap">
-                  <div class="hidden sm:block w-full py-3 pl-2 text-left sm:w-[8%] sm:text-left bg-blue-200 text-white text-sm font-bold">Línea</div>
-                  <div class="hidden sm:block w-full py-3 text-left sm:w-[8%] sm:text-center bg-blue-200 text-white text-sm font-bold">Código</div>
-                  <div class="hidden sm:block w-full py-3 text-left sm:w-[8%] sm:text-left bg-blue-200 text-white text-sm font-bold">Categoría</div>
-                  <div class="hidden sm:block w-full py-3 text-left sm:w-[20%] sm:text-left bg-blue-200 text-white text-sm font-bold">Nombre</div>
-                  <div class="hidden sm:block w-full py-3 text-left sm:w-[30%] sm:text-left bg-blue-200 text-white text-sm font-bold">Descripción</div>
-                  <div class="hidden sm:block w-full py-3 text-left sm:w-[8%] sm:text-center bg-blue-200 text-white text-sm font-bold">Peso</div>
-                  <div class="hidden sm:block w-full py-3 text-left sm:w-[8%] sm:text-center bg-blue-200 text-white text-sm font-bold">Tpo. Curado</div>
-                  <div class="hidden sm:block w-full py-3 text-left sm:w-[10%] sm:text-center bg-blue-200 text-white text-sm font-bold">Acciones</div>
+                  <div
+                        class="hidden sm:block w-full py-3 pl-2 text-left sm:w-[8%] sm:text-left bg-blue-100 text-white text-sm font-bold">
+                        Línea</div>
+                  <div
+                        class="hidden sm:block w-full py-3 text-left sm:w-[8%] sm:text-center bg-blue-100 text-white text-sm font-bold">
+                        Código</div>
+                  <div
+                        class="hidden sm:block w-full py-3 text-left sm:w-[8%] sm:text-left bg-blue-100 text-white text-sm font-bold">
+                        Categoría</div>
+                  <div
+                        class="hidden sm:block w-full py-3 text-left sm:w-[20%] sm:text-left bg-blue-100 text-white text-sm font-bold">
+                        Nombre</div>
+                  <div
+                        class="hidden sm:block w-full py-3 text-left sm:w-[30%] sm:text-left bg-blue-100 text-white text-sm font-bold">
+                        Descripción</div>
+                  <div
+                        class="hidden sm:block w-full py-3 text-left sm:w-[8%] sm:text-center bg-blue-100 text-white text-sm font-bold">
+                        Peso</div>
+                  <div
+                        class="hidden sm:block w-full py-3 text-left sm:w-[8%] sm:text-center bg-blue-100 text-white text-sm font-bold">
+                        Tpo. Curado</div>
+                  <div
+                        class="hidden sm:block w-full py-3 text-left sm:w-[10%] sm:text-center bg-blue-100 text-white text-sm font-bold">
+                        Acciones</div>
             </div>
             @foreach ($listProductos as $producto)
-                  <div class="flex flex-wrap border-b-[1px] hover:bg-gold-100 hover:cursor-pointer">
-                        <div class="w-full flex mx-1 sm:mx-0 items-center text-left sm:w-[8%] sm:text-left text-sm">{{ $producto->linea }}</div>
-                        <div class="w-full flex mx-1 sm:mx-0 items-center justify-start sm:justify-center sm:w-[8%] sm:text-center  text-sm">{{ $producto->codigo }}</div>
-                        <div class="w-full flex mx-1 sm:mx-0 items-center text-left sm:w-[8%] sm:text-left  text-sm">{{ $producto->categoria }}</div>
-                        <div class="w-full flex mx-1 sm:mx-0 items-center text-left sm:w-[20%] sm:text-left  text-sm">{{ $producto->nombre }}</div>
-                        <div class="w-full flex mx-1 sm:mx-0 items-center text-left sm:w-[30%] sm:text-left  text-sm">{{ $producto->descrip }}</div>
-                        <div class="w-full flex mx-1 sm:mx-0 items-center justify-start sm:justify-center sm:w-[8%] sm:text-center  text-sm">{{ $producto->peso }}</div>
-                        <div class="w-full flex mx-1 sm:mx-0 items-center justify-start sm:justify-center sm:w-[8%] sm:text-center  text-sm">{{ $producto->tiempoCurado }}</div>
-                        <div class="w-full flex items-center justify-center sm:w-[10%] sm:text-center text-sm">
-                              <div class="flex justify-center">
-                                    @livewire('componentes.paneladmin.productover', ['idProducto'=>$producto->id], key('01_'.uniqid()))
-                                    @livewire('componentes.paneladmin.productoedit', ['idProducto'=>$producto->id], key('02_'.uniqid()))
-                                    @livewire('componentes.paneladmin.productoeliminar', ['idProducto'=>$producto->id], key('03_'.uniqid()))
-                              </div>
+            <div class="flex flex-wrap border-b-[1px] hover:bg-gold-100 hover:cursor-pointer">
+                  <div class="w-full flex mx-1 sm:mx-0 items-center text-left sm:w-[8%] sm:text-left text-sm">{{
+                        $producto->linea }}</div>
+                  <div
+                        class="w-full flex mx-1 sm:mx-0 items-center justify-start sm:justify-center sm:w-[8%] sm:text-center  text-sm">
+                        {{ $producto->codigo }}</div>
+                  <div class="w-full flex mx-1 sm:mx-0 items-center text-left sm:w-[8%] sm:text-left  text-sm">{{
+                        $producto->categoria }}</div>
+                  <div class="w-full flex mx-1 sm:mx-0 items-center text-left sm:w-[20%] sm:text-left  text-sm">{{
+                        $producto->nombre }}</div>
+                  <div class="w-full flex mx-1 sm:mx-0 items-center text-left sm:w-[30%] sm:text-left  text-sm">{{
+                        $producto->descrip }}</div>
+                  <div
+                        class="w-full flex mx-1 sm:mx-0 items-center justify-start sm:justify-center sm:w-[8%] sm:text-center  text-sm">
+                        {{ $producto->peso }}</div>
+                  <div
+                        class="w-full flex mx-1 sm:mx-0 items-center justify-start sm:justify-center sm:w-[8%] sm:text-center  text-sm">
+                        {{ $producto->tiempoCurado }}</div>
+                  <div class="w-full flex items-center justify-center sm:w-[10%] sm:text-center text-sm">
+                        <div class="flex justify-center">
+                              @livewire('componentes.paneladmin.productover', ['idProducto'=>$producto->id],
+                              key('01_'.uniqid()))
+                              @livewire('componentes.paneladmin.productoedit', ['idProducto'=>$producto->id],
+                              key('02_'.uniqid()))
+                              @livewire('componentes.paneladmin.productoeliminar', ['idProducto'=>$producto->id],
+                              key('03_'.uniqid()))
                         </div>
                   </div>
+            </div>
             @endforeach
 
             <div class="mt-3">
--- resources/views/livewire/componentes/paneladmin/listrecetas.blade.php
+++ resources/views/livewire/componentes/paneladmin/listrecetas.blade.php
@@ -1,35 +1,38 @@
 <div>
 
       <div class="container mx-auto">
-            <div class="flex justify-between items-center">
-                  <span class="text-[2.5rem] font-extrabold">RECETAS</span>
+            <div class="flex items-center justify-between">
+                  <span class="text-[2.5rem] font-extrabold eucrosia">Recetas</span>
                   @livewire('componentes.paneladmin.recetanueva')
             </div>
             <table class="table-fixed">
                   <thead>
                         <tr>
-                              <th class="text-left bg-blue-200 text-white w-[40%] text-sm">Nombre</th>
-                              <th class="text-left bg-blue-200 text-white w-[20%] text-sm">Tiempo de preparación</th>
-                              <th class="text-left bg-blue-200 text-white w-[20%] text-sm">Tiempo de cocción</th>
-                              <th class="text-left bg-blue-200 text-white w-[20%] text-sm">Tiempo total</th>
-                              <th class="bg-blue-200 text-white">Acciones</th>
+                              <th class="text-left bg-blue-100 text-white w-[40%] text-sm py-3 pl-2">Nombre</th>
+                              <th class="text-left bg-blue-100 text-white w-[20%] text-sm">Tiempo de preparación</th>
+                              <th class="text-left bg-blue-100 text-white w-[20%] text-sm">Tiempo de cocción</th>
+                              <th class="text-left bg-blue-100 text-white w-[20%] text-sm">Tiempo total</th>
+                              <th class="text-white bg-blue-100">Acciones</th>
                         </tr>
                   </thead>
                   <tbody>
                         @foreach ($listRecetas as $receta)
-                              <tr class="hover:bg-gold-100 hover:cursor-pointer">
-                                    <td class="py-2 border-b-[1px] text-left text-xs">{{ $receta->nombre }}</td>
-                                    <td class="py-2 border-b-[1px] text-left text-xs">{{ $receta->tiempoPreparacion }}</td>
-                                    <td class="py-2 border-b-[1px] text-left text-xs">{{ $receta->tiempoCoccion }}</td>
-                                    <td class="py-2 border-b-[1px] text-left text-xs">{{ $receta->tiempoTotal }}</td>
-                                    <td class="py-2 border-b-[1px]">
-                                          <div class="flex justify-center items-center">
-                                                @livewire('componentes.paneladmin.recetaver', ['idReceta'=>$receta->id], key('01_'.uniqid()))
-                                                @livewire('componentes.paneladmin.recetaedit', ['idReceta'=>$receta->id], key('02_'.uniqid()))
-                                                @livewire('componentes.paneladmin.recetaeliminar', ['idReceta'=>$receta->id], key('03_'.uniqid()))
-                                          </div>
-                                    </td>
-                              </tr>
+                        <tr class="hover:bg-gold-100 hover:cursor-pointer">
+                              <td class="py-2 border-b-[1px] text-left text-sm">{{ $receta->nombre }}</td>
+                              <td class="py-2 border-b-[1px] text-left text-sm">{{ $receta->tiempoPreparacion }}</td>
+                              <td class="py-2 border-b-[1px] text-left text-sm">{{ $receta->tiempoCoccion }}</td>
+                              <td class="py-2 border-b-[1px] text-left text-sm">{{ $receta->tiempoTotal }}</td>
+                              <td class="py-2 border-b-[1px]">
+                                    <div class="flex items-center justify-center">
+                                          @livewire('componentes.paneladmin.recetaver', ['idReceta'=>$receta->id],
+                                          key('01_'.uniqid()))
+                                          @livewire('componentes.paneladmin.recetaedit', ['idReceta'=>$receta->id],
+                                          key('02_'.uniqid()))
+                                          @livewire('componentes.paneladmin.recetaeliminar', ['idReceta'=>$receta->id],
+                                          key('03_'.uniqid()))
+                                    </div>
+                              </td>
+                        </tr>
                         @endforeach
                   </tbody>
             </table>
